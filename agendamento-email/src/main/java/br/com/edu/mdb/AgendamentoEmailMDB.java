bbd8a6507c691c884bfcd23a7e531b063fc8d39d
DUDU <DUDU@DUDU-PC>
2021-05-11 14:09:45 -0300

    Agendamento email

---
+++ agendamento-email/src/main/java/br/com/edu/mdb/AgendamentoEmailMDB.java
@@ -0,0 +1,30 @@
+package br.com.edu.mdb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import br.com.edu.entidade.AgendamentoEmail;
+import br.com.edu.servico.AgendamentoEmailServico;
+
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:/jms/queue/EmailQueue"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") })
+public class AgendamentoEmailMDB implements MessageListener {
+
+	@Inject
+	private AgendamentoEmailServico agendamentoEmailServico;
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			AgendamentoEmail agendamentoEmail = message.getBody(AgendamentoEmail.class);
+			agendamentoEmailServico.enviar(agendamentoEmail);
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
